54f683cb988c64ad0b6529e8f24827dbc78ea290
Shalev Tamir <shalevtamir1009@gmail.com>
2023-09-21 16:38:06 +0300

    removed unnecessary files

--- services/classification/regularization_models/I_regularization_model.py
+++
@@ -1,7 +0,0 @@
-from abc import ABC, abstractmethod
-
-
-class IRegularizationModel(ABC):
-    @abstractmethod
-    def regularize(self,embedded_group1: list[list[float]], embedded_group2: list[float]):
-        pass
